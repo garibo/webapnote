10d1d54f4939f6cebc83a08150f6791ae92d161f
Javier Diaz <javier.diaz.internaut@gmail.com>
2014-11-22 13:41:19 -0600

    Change to Commit

--- application/config/database.php
+++ application/config/database.php
@@ -46,13 +46,12 @@
 */
 
 $active_group = 'default';
-//$active_group = 'production';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'Ignition19852';
-$db['default']['database'] = 'ci_apnote';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
@@ -64,22 +63,6 @@ $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
-
-$db['production']['hostname'] = 'localhost';
-$db['production']['username'] = 'recycler_webapp';
-$db['production']['password'] = 'CeRcAtRoVa#@';
-$db['production']['database'] = 'recycler_apnote';
-$db['production']['dbdriver'] = 'mysql';
-$db['production']['dbprefix'] = '';
-$db['production']['pconnect'] = TRUE;
-$db['production']['db_debug'] = TRUE;
-$db['production']['cache_on'] = FALSE;
-$db['production']['cachedir'] = '';
-$db['production']['char_set'] = 'utf8';
-$db['production']['dbcollat'] = 'utf8_general_ci';
-$db['production']['swap_pre'] = '';
-$db['production']['autoinit'] = TRUE;
-$db['production']['stricton'] = FALSE;
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
